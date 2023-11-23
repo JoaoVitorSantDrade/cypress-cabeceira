33fbfaa836a0baf474c5dfb3fe6eee13ef3a9c8f
JoaoVitorSantDrade <jvitorsantdrade@gmail.com>
2023-11-23 15:40:31 -0300

    spec cy js changes

--- cypress/e2e/spec.cy.js
+++ cypress/e2e/spec.cy.js
@@ -1,11 +1,11 @@
 describe('Visit login page', () => {
   it('passes', () => {
-    cy.visit('http://localhost:5173/login')
+    cy.visit(Cypress.env('url') + '/login')
   })
 })
 
 describe('Visit register page', () => {
   it('passes', () => {
-    cy.visit('http://localhost:5173/register')
+    cy.visit(Cypress.env('url') + '/register')
   })
 })
